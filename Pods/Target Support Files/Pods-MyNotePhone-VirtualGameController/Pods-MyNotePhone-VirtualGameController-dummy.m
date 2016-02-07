99a90e6d4530ce6384491399539ffa0befc5c53c
Jake Saferstein <jakesafo@gmail.com>
2016-02-07 01:01:47 -0500

    .gitignore is now working

---
+++ Pods/Target Support Files/Pods-MyNotePhone-VirtualGameController/Pods-MyNotePhone-VirtualGameController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyNotePhone_VirtualGameController : NSObject
+@end
+@implementation PodsDummy_Pods_MyNotePhone_VirtualGameController
+@end
